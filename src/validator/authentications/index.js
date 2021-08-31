97d8127945d82fc02a982680370dfe9734af94c6
ferdiany19 <ferdianyuliansyah19@gmai.com>
2021-08-31 08:08:10 +0700

    authentication implemented

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,29 @@
+const {
+  PostAuthenticationPayloadSchema,
+  PutAuthenticationPayloadSchema,
+  DeleteAuthenticationPayloadSchema,
+} = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const AuthenticationValidator = {
+  validatePostAuthentication: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validatePutAuthentication: (payload) => {
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeleteAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AuthenticationValidator;
